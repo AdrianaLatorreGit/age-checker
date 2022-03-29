43aacb74de1a3638511802389a28c3c7b56c47e8
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-03-29 17:54:34 -0300

    link image-folder

--- script.js
+++ script.js
@@ -2,50 +2,49 @@
 function verificar() {
   var data = new Date();
   var ano = data.getFullYear();
-  var fano = document.getElementById('txtano');
-  var res = document.getElementById('res');
+  var fano = document.getElementById("txtano");
+  var res = document.getElementById("res");
   if (Number(fano.value.lenght) == 0 || Number(fano.value) > ano) {
-    window.alert('Verifique os dados e tente novamente');
+    window.alert("Verifique os dados e tente novamente");
   } else {
-    var fsex = document.getElementsByName('radsex');
+    var fsex = document.getElementsByName("radsex");
     var idade = ano - Number(fano.value);
-    var genero = '';
-    var img = document.createElement('img');
-    img.setAttribute('id', 'foto');
+    var genero = "";
+    var img = document.createElement("img");
+    img.setAttribute("id", "foto");
     if (fsex[0].checked) {
-      genero = 'Homem';
+      genero = "Homem";
       if (idade >= 0 && idade < 10) {
         //criança
-        img.setAttribute('src', 'menino.png');
+        img.setAttribute("src", "images/menino.png");
       } else if (idade >= 10 && idade < 18) {
         //jovem
-        img.setAttribute('src', 'jovemh.png');
+        img.setAttribute("src", "images/jovemh.png");
       } else if (idade >= 18 && idade < 60) {
         //adulto
-        img.setAttribute('src', 'homem.png');
+        img.setAttribute("src", "images/homem.png");
       } else {
         //idoso
-        img.setAttribute('src', 'idoso.png');
+        img.setAttribute("src", "images/idoso.png");
       }
     } else {
-      genero = 'Mulher';
+      genero = "Mulher";
       if (idade >= 0 && idade < 10) {
         //criança
-        img.setAttribute('src', 'menina.png');
+        img.setAttribute("src", "images/menina.png");
       } else if (idade >= 10 && idade < 18) {
         //jovem
-        img.setAttribute('src', 'jovemm.png');
+        img.setAttribute("src", "images/jovemm.png");
       } else if (idade >= 18 && idade < 60) {
         //adulto
-        img.setAttribute('src', 'mulher.png');
+        img.setAttribute("src", "images/mulher.png");
       } else {
         //idoso
-        img.setAttribute('src', 'idosa.png');
+        img.setAttribute("src", "images/idosa.png");
       }
     }
-    res.style.textAlign = 'center';
-    res.innerHTML = 'Detectamos ' + genero + ' com ' + idade + ' anos';
+    res.style.textAlign = "center";
+    res.innerHTML = "Detectamos " + genero + " com " + idade + " anos";
     res.appendChild(img);
   }
 }
-
